a6a2fb6d19a882c6d4202a4e67da38468af1f567
earcanal <paul@miraclefish.com>
2011-09-28 22:57:25 +0100

    Mix in some Moose.

--- lib/FBW/Schema/Result/Player.pm
+++ lib/FBW/Schema/Result/Player.pm
@@ -1,5 +1,24 @@
 package FBW::Schema::Result::Player;
-use parent qw/DBIx::Class::Core/;
+use Moose;
+extends qw/DBIx::Class::Core/;
+
+# Mixing Moose/DBIC example
+# http://code2.0beta.co.uk/reaction/svn/Reaction/0.001/trunk/t/lib/RTest/TestDB/Foo.pm
+
+#my $meta = __PACKAGE__->meta;
+#for my $attr ( $meta->get_all_attributes ) {
+#      print $attr->name, "\n";
+#  }
+
+has 'id'     => (is => 'ro', isa => 'Int');
+has 'number' => (is => 'ro', isa => 'Int');
+has 'name'   => (is => 'rw', isa => 'String');
+has 'aka'    => (
+		 isa    => 'Array',
+		 reader => 'get_aka',
+		 writer => 'set_aka'
+		 );
+
 __PACKAGE__->table('player');
 __PACKAGE__->add_columns(qw/id number name/);
 __PACKAGE__->set_primary_key('id');
@@ -10,6 +29,8 @@ __PACKAGE__->has_many(team => 'FBW::Schema::Result::TeamPlayer',
 __PACKAGE__->many_to_many(teams => 'team','team');
 __PACKAGE__->has_many(stat => 'FBW::Schema::Result::Stat',
 		      {'foreign.player_id' => 'self.id'});
+
+around 'get_aka' => sub { $_[1]->search_related('aka')->all };
 
 sub manager {
   my($self,$f) = @_;
